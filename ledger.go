188ea3dd0f33f5a6df4e857116b3896573e32fce
Kenta Iwasaki <dranithix@gmail.com>
2018-09-27 04:26:40 +0800

    ledger, db: only write accepted status of tx to the db should its accepted status change locally on the ledger

--- ledger.go
+++ ledger.go
@@ -1,6 +1,7 @@
 package wavelet
 
 import (
+	"github.com/lytics/hll"
 	"github.com/perlin-network/graph/conflict"
 	"github.com/perlin-network/graph/database"
 	"github.com/perlin-network/graph/graph"
@@ -60,32 +61,119 @@ func (ledger *Ledger) processTransactions() {
 		start = 0
 	}
 
-	accepted := make(map[uint64][]string)
+	accepted := make(map[uint64]map[string]bool)
 
 	for depth := start; depth <= threshold; depth++ {
 		ledger.Store.ForEachDepth(depth, func(index uint64, symbol string) error {
-			if ledger.Resolver.IsAccepted(symbol) {
-				accepted[depth] = append(accepted[depth], symbol)
+			status := ledger.IsAccepted(symbol)
+
+			acceptedBytes, err := ledger.Store.Get(merge(BucketAccepted, writeBytes(symbol)))
+
+			// Write the accepted status of transactions only if it ever changed.
+			if accepted := readBoolean(acceptedBytes); err == nil && status == accepted {
+				return nil
 			}
+
+			if accepted[depth] == nil {
+				accepted[depth] = make(map[string]bool)
+			}
+
+			accepted[depth][symbol] = status
 			return nil
 		})
 	}
 
-	// Save accepted transactions to the database.
-	for depth, symbols := range accepted {
-		for _, symbol := range symbols {
-			key := merge(BucketAccepted, writeUint64(depth), writeBytes(symbol))
+	totalAccepted := 0
 
-			err := ledger.Store.Put(key, writeBoolean(true))
+	// Save accepted transactions to the database.
+	for _, symbols := range accepted {
+		for symbol, accepted := range symbols {
+			key := merge(BucketAccepted, writeBytes(symbol))
+
+			err := ledger.Store.Put(key, writeBoolean(accepted))
 			if err != nil {
 				log.Error().Err(err).Msg("failed to write tx being accepted to db")
+			}
+
+			if accepted {
+				totalAccepted++
 			}
 		}
 	}
 
-	if len(accepted) > 0 {
-		log.Info().Msgf("Accepted %d transactions.", len(accepted))
+	if totalAccepted > 0 {
+		log.Info().Msgf("Accepted %d transactions.", totalAccepted)
 	}
+}
+
+func (ledger *Ledger) IsAccepted(symbol string) bool {
+	tx, err := ledger.Store.GetBySymbol(symbol)
+	if err != nil {
+		return false
+	}
+
+	set, err := ledger.Resolver.GetConflictSet(tx.Sender, tx.Nonce)
+	if err != nil {
+		return false
+	}
+
+	// Condition 2 for being accepted. (beta 2 = 150)
+	if set.Count > system.Beta2 {
+		return true
+	}
+
+	visited := make(map[string]struct{})
+	queue := tx.Parents
+
+	// Check for Condition 1.
+	for len(queue) > 0 {
+		popped := queue[0]
+		queue = queue[1:]
+
+		tx, err = ledger.Store.GetBySymbol(popped)
+		if err != nil {
+			return false
+		}
+
+		set, err = ledger.Resolver.GetConflictSet(tx.Sender, tx.Nonce)
+		if err != nil {
+			return false
+		}
+
+		transactions := new(hll.Hll)
+		err = transactions.UnmarshalPb(set.Transactions)
+
+		// If we fail to unmarshal the HyperLogLog++ counter, then consider the transaction
+		// not accepted to be safe. This implies the conflict set's bytes (in the database)
+		// are corrupt.
+		if err != nil {
+			return false
+		}
+
+		conflicting := transactions.Cardinality() > 1
+
+		// Condition 1 for being accepted. (beta 1 = 10)
+		if conflicting || ledger.Graph.CountAscendants(symbol, system.Beta1+1) <= system.Beta1 {
+			return false
+		}
+
+		acceptedBytes, err := ledger.Store.Get(merge(BucketAccepted, writeBytes(popped)))
+
+		// If the transaction appears to have already been accepted, stop traversing through
+		// its descendants.
+		if accepted := readBoolean(acceptedBytes); err == nil && accepted {
+			continue
+		}
+
+		for _, parent := range tx.Parents {
+			if _, seen := visited[parent]; !seen {
+				queue = append(queue, parent)
+				visited[parent] = struct{}{}
+			}
+		}
+	}
+
+	return true
 }
 
 // RespondToQuery provides a response should we be selected as one of the K peers
