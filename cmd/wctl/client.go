a82069b42ea95f05a1de3344f3f0e76e5b49b917
doug <39219081+doug-perlin@users.noreply.github.com>
2018-10-01 23:43:12 +0800

    wctl: initial implementation code

---
+++ cmd/wctl/client.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"bytes"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/perlin-network/perlin-go/security"
+	"github.com/perlin-network/wavelet/api"
+)
+
+// Client represents a Perlin Ledger client.
+type Client struct {
+	Config       ClientConfig
+	SessionToken string
+	KeyPair      *security.KeyPair
+}
+
+// ClientConfig represents a Perlin Ledger client config.
+type ClientConfig struct {
+	RemoteAddr string
+	PrivateKey string
+	UseHTTPS   bool
+}
+
+// SessionResponse represents the response from a session call
+type SessionResponse struct {
+	Token string  `json:"token"`
+}
+
+// NewClient creates a new Perlin Ledger client from a config.
+func NewClient(config ClientConfig) (*Client, error) {
+	keys, err := security.FromPrivateKey(security.SignaturePolicy, config.PrivateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		Config:  config,
+		KeyPair: keys,
+	}, nil
+}
+
+// Init will initialize a client.
+func (c *Client) Init() error {
+	millis := time.Now().Unix() * 1000
+	authStr := fmt.Sprintf("%s%d", security.SessionInitSigningPrefix, millis)
+	sig := security.Sign(c.KeyPair.PrivateKey, []byte(authStr))
+
+	creds := api.Credentials{
+		PublicKey:  hex.EncodeToString(c.KeyPair.PublicKey),
+		TimeMillis: millis,
+		Sig:        hex.EncodeToString(sig),
+	}
+
+	resp := SessionResponse{}
+
+	err := c.Request("/session/init", &creds, &resp)
+	if err != nil {
+		return err
+	}
+	c.SessionToken = resp.Token
+	return nil
+}
+
+// Request will make a request to a given path, with a given body and return result in out.
+func (c *Client) Request(path string, body, out interface{}) error {
+	prot := "http"
+	if c.Config.UseHTTPS {
+		prot = "https"
+	}
+	u, err := url.Parse(fmt.Sprintf("%s://%s%s", prot, c.Config.RemoteAddr, path))
+	if err != nil {
+		return err
+	}
+
+	rawBody, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+
+	req := &http.Request{
+		Method: "POST",
+		URL:    u,
+		Header: map[string][]string{
+			"X-Session-Token": []string{c.SessionToken},
+		},
+		Body: ioutil.NopCloser(bytes.NewReader(rawBody)),
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return fmt.Errorf("got an error code %s: %s", resp.Status, string(data))
+	}
+
+	if out == nil {
+		return nil
+	}
+	err = json.Unmarshal(data, out)
+	return err
+}
---
+++ cmd/wctl/main.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/perlin-network/wavelet/cmd/utils"
+	"github.com/perlin-network/wavelet/log"
+	"github.com/urfave/cli"
+)
+
+func main() {
+
+	app := cli.NewApp()
+
+	app.Name = "wctl"
+	app.Author = "Perlin Network"
+	app.Email = "support@perlin.net"
+	app.Version = utils.Version
+	app.Usage = "a cli client to interact with the wavelet node"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "remote, r",
+			Value: "localhost:3001",
+			Usage: "remote address `HOST`.",
+		},
+		cli.StringFlag{
+			Name:  "privkey, p",
+			Value: "",
+			Usage: "private key (hex) `PORT`.",
+		},
+	}
+
+	cli.VersionPrinter = func(c *cli.Context) {
+		fmt.Printf("Version: %s\n", c.App.Version)
+		fmt.Printf("Go Version: %s\n", utils.GoVersion)
+		fmt.Printf("Git Commit: %s\n", utils.GitCommit)
+		fmt.Printf("Built: %s\n", c.App.Compiled.Format(time.ANSIC))
+	}
+
+	app.Action = runAction
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal().Err(err).Msg("Failed to parse configuration/command-line arugments.")
+	}
+}
+
+func runAction(c *cli.Context) {
+	remoteAddr := c.String("remote")
+	privateKey := c.String("privKey")
+
+	if len(remoteAddr) == 0 {
+		log.Fatal().Msg("remote flag is missing")
+	}
+
+	client, err := NewClient(ClientConfig{
+		RemoteAddr: remoteAddr,
+		PrivateKey: privateKey,
+		UseHTTPS:   false,
+	})
+	if err != nil {
+		log.Fatal().Err(err).Msg("")
+	}
+
+	err = client.Init()
+	if err != nil {
+		log.Fatal().Err(err).Msg("")
+	}
+
+	log.Info().Str("SessionToken", client.SessionToken).Msg("")
+
+	cmd := ([]string)(c.Args())
+	if err != nil {
+		log.Fatal().Err(err).Msg("")
+	}
+
+	if len(cmd) == 0 {
+		log.Fatal().Msg("Missing command argument")
+	}
+
+	switch cmd[0] {
+	case "wallet":
+		recipient := "71e6c9b83a7ef02bae6764991eefe53360a0a09be53887b2d3900d02c00a3858"
+		if len(cmd) >= 2 {
+			recipient = cmd[1]
+		}
+		var ret map[string][]byte
+		if err := c.Request("/account/load", id, &ret); err != nil {
+
+		}
+		log.Fatal().Msg("TODO: not implemented")
+	case "pay":
+
+		tag := "transfer"
+		recipient := "71e6c9b83a7ef02bae6764991eefe53360a0a09be53887b2d3900d02c00a3858"
+		amount := 1
+
+		if len(cmd) >= 2 {
+			recipient = cmd[1]
+		}
+
+		if len(cmd) >= 3 {
+			amount, err = strconv.Atoi(cmd[2])
+			if err != nil {
+				log.Fatal().Err(err).Msg("Failed to convert payment amount to an uint64.")
+			}
+		}
+
+		transfer := struct {
+			Recipient string `json:"recipient"`
+			Amount    uint64 `json:"amount"`
+		}{
+			Recipient: recipient,
+			Amount:    uint64(amount),
+		}
+
+		payload, err := json.Marshal(transfer)
+		if err != nil {
+			log.Fatal().Err(err).Msg("Failed to marshal transfer payload.")
+		}
+
+		if err := client.Request("/transaction/send", struct {
+			Tag     string `json:"tag"`
+			Payload []byte `json:"payload"`
+		}{
+			Tag:     tag,
+			Payload: payload,
+		}, nil); err != nil {
+			log.Fatal().Err(err).Msg("Failed to send pay command.")
+		}
+	case "contract":
+		recipient := "71e6c9b83a7ef02bae6764991eefe53360a0a09be53887b2d3900d02c00a3858"
+		contractPath := ""
+
+		if len(cmd) < 2 {
+			recipient = cmd[1]
+		}
+
+		if len(cmd) >= 3 {
+			contractPath = cmd[2]
+		}
+
+		bytes, err := ioutil.ReadFile(contractPath)
+		if err != nil {
+			log.Fatal().
+				Err(err).
+				Str("path", contractPath).
+				Msg("Failed to find/load the smart contract code from the given path.")
+		}
+
+		contract := struct {
+			Code string `json:"code"`
+		}{
+			Code: base64.StdEncoding.EncodeToString(bytes),
+		}
+
+		payload, err := json.Marshal(contract)
+		if err != nil {
+			log.Fatal().Err(err).Msg("Failed to marshal smart contract deployment payload.")
+		}
+
+		log.Fatal().Msg("TODO: not implemented")
+	case "stats_reset":
+		res := new(interface{})
+		if err := client.Request("/stats/reset", struct{}{}, res); err != nil {
+			log.Fatal().Err(err).Msg("")
+		}
+		jsonOut, _ := json.Marshal(res)
+		fmt.Printf("%s\n", jsonOut)
+	case "stats_summary":
+		res := new(interface{})
+		if err := client.Request("/stats/summary", struct{}{}, res); err != nil {
+			log.Fatal().Err(err).Msg("")
+		}
+		jsonOut, _ := json.Marshal(res)
+		fmt.Printf("%s\n", jsonOut)
+	default:
+		log.Fatal().Msgf("unknown command: %s", cmd[0])
+	}
+}
