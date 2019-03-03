cdc1aecea9e2bc561807b6009436316be4a2db12
Ahmad Muzakkir <ahmad@perlin.net>
2019-03-03 22:18:08 +0800

    api/test: add tests for /accounts/{id} and /contract/{id}

--- api/mod_test.go
+++ api/mod_test.go
@@ -7,13 +7,19 @@ import (
 	"fmt"
 	"github.com/perlin-network/noise/identity/ed25519"
 	"github.com/perlin-network/noise/signature/eddsa"
+	"github.com/perlin-network/wavelet"
+	"github.com/perlin-network/wavelet/store"
 	"github.com/perlin-network/wavelet/sys"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
 )
+
+var genesisPath = "../cmd/wavelet/config/genesis.json"
 
 func TestInitSession(t *testing.T) {
 	randomKeyPair := ed25519.RandomKeys()
@@ -42,9 +48,7 @@ func TestInitSession(t *testing.T) {
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			body, err := json.Marshal(tc.req)
-			if err != nil {
-				t.Fatal(err)
-			}
+			assert.NoError(t, err)
 
 			request := httptest.NewRequest("POST", "/session/init", bytes.NewReader(body))
 			request.Header.Add("Content-Type", "application/json")
@@ -53,14 +57,11 @@ func TestInitSession(t *testing.T) {
 
 			hub.router.ServeHTTP(w, request)
 
-			raw, err := ioutil.ReadAll(w.Body)
-			if err != nil {
-				t.Fatal(err)
-			}
+			_, err = ioutil.ReadAll(w.Body)
+			assert.Nil(t, err)
 
-			if w.Code != tc.wantCode {
-				t.Fatalf("expected status code %d, found %d: %s", tc.wantCode, w.Code, string(raw))
-			}
+			assert.Equal(t, tc.wantCode, w.Code, "status code")
+
 		})
 	}
 }
@@ -97,14 +98,10 @@ func TestListTransaction(t *testing.T) {
 
 			hub.router.ServeHTTP(w, request)
 
-			raw, err := ioutil.ReadAll(w.Body)
-			if err != nil {
-				t.Fatal(err)
-			}
+			_, err := ioutil.ReadAll(w.Body)
+			assert.Nil(t, err)
 
-			if w.Code != tc.wantCode {
-				t.Fatalf("expected status code %d, found %d: %s", tc.wantCode, w.Code, string(raw))
-			}
+			assert.Equal(t, tc.wantCode, w.Code, "status code")
 		})
 	}
 }
@@ -114,23 +111,24 @@ func TestGetTransaction(t *testing.T) {
 	hub.setupRouter()
 
 	sess, err := hub.registry.newSession()
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 
 	tests := []struct {
 		name         string
 		sessionToken string
 		id           string
 		wantCode     int
-		wantError    string
+		wantResponse interface{}
 	}{
 		{
 			name:         "invalid id length",
 			sessionToken: sess.id,
 			id:           "1c331c1d",
 			wantCode:     http.StatusBadRequest,
-			wantError:    fmt.Sprintf("transaction ID must be %d bytes long", sys.TransactionIDSize),
+			wantResponse: &ErrResponse{
+				StatusText: "Bad request.",
+				ErrorText:  fmt.Sprintf("transaction ID must be %d bytes long", sys.TransactionIDSize),
+			},
 		},
 	}
 
@@ -147,23 +145,12 @@ func TestGetTransaction(t *testing.T) {
 			hub.router.ServeHTTP(w, request)
 
 			raw, err := ioutil.ReadAll(w.Body)
-			if err != nil {
-				t.Fatal(err)
-			}
+			assert.Nil(t, err)
 
-			var response ErrResponse
-			err = json.Unmarshal(raw, &response)
-			if err != nil {
-				t.Fatal(err)
-			}
+			assert.Equal(t, tc.wantCode, w.Code, "status code")
 
-			if w.Code != tc.wantCode {
-				t.Fatalf("expected status code %d, found %d: %s", tc.wantCode, w.Code, string(raw))
-			}
-
-			if response.ErrorText != tc.wantError {
-				t.Fatalf("expected error %s, found %s: %s", tc.wantError, response.ErrorText, string(raw))
-
+			if tc.wantResponse != nil {
+				assert.NoError(t, compareJson(tc.wantResponse, raw))
 			}
 		})
 	}
@@ -193,9 +180,7 @@ func TestSendTransaction(t *testing.T) {
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			reqBody, err := json.Marshal(tc.req)
-			if err != nil {
-				t.Fatal(err)
-			}
+			assert.NoError(t, err)
 
 			request := httptest.NewRequest("POST", "/tx/send", bytes.NewReader(reqBody))
 
@@ -207,13 +192,155 @@ func TestSendTransaction(t *testing.T) {
 
 			hub.router.ServeHTTP(w, request)
 
-			raw, err := ioutil.ReadAll(w.Body)
-			if err != nil {
-				t.Fatal(err)
-			}
+			_, err = ioutil.ReadAll(w.Body)
+			assert.Nil(t, err)
 
-			if w.Code != tc.wantCode {
-				t.Fatalf("expected status code %d, found %d: %s", tc.wantCode, w.Code, string(raw))
+			assert.Equal(t, tc.wantCode, w.Code, "status code")
+		})
+	}
+}
+
+func TestGetAccount(t *testing.T) {
+	hub := &Hub{registry: newSessionRegistry()}
+	hub.setupRouter()
+
+	sess, err := hub.registry.newSession()
+	assert.NoError(t, err)
+
+	hub.ledger = createLedger()
+
+	idHex := "1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d"
+	idBytes, err := hex.DecodeString(idHex)
+	assert.NoError(t, err)
+
+	var id32 [sys.PublicKeySize]byte
+	copy(id32[:], idBytes)
+
+	hub.ledger.WriteAccountBalance(id32, 10)
+	hub.ledger.WriteAccountStake(id32, 11)
+	hub.ledger.WriteAccountContractNumPages(id32, 12)
+
+	tests := []struct {
+		name         string
+		url          string
+		wantCode     int
+		wantError    string
+		wantResponse interface{}
+	}{
+		{
+			name:     "missing id",
+			url:      "/accounts/",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "id not hex",
+			url:      "/accounts/-----",
+			wantCode: http.StatusBadRequest,
+		},
+		{
+			name:     "invalid id length",
+			url:      "/accounts/1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d",
+			wantCode: http.StatusBadRequest,
+		},
+		{
+			name:     "valid id",
+			url:      "/accounts/" + idHex,
+			wantCode: http.StatusOK,
+			wantResponse: &Account{
+				PublicKey:  idHex,
+				Balance:    10,
+				Stake:      11,
+				IsContract: true,
+				NumPages:   12,
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			request := httptest.NewRequest("GET", tc.url, nil)
+			request.Header.Add(HeaderSessionToken, sess.id)
+
+			w := httptest.NewRecorder()
+
+			hub.router.ServeHTTP(w, request)
+
+			raw, err := ioutil.ReadAll(w.Body)
+			assert.NoError(t, err)
+
+			assert.Equal(t, tc.wantCode, w.Code, "status code")
+
+			if tc.wantResponse != nil {
+				assert.NoError(t, compareJson(tc.wantResponse, raw))
+			}
+		})
+	}
+}
+
+func TestGetContractCode(t *testing.T) {
+	hub := &Hub{registry: newSessionRegistry()}
+	hub.setupRouter()
+
+	sess, err := hub.registry.newSession()
+	assert.NoError(t, err)
+
+	hub.ledger = createLedger()
+
+	idHex := "1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d"
+	idBytes, err := hex.DecodeString(idHex)
+	assert.NoError(t, err)
+
+	var id32 [sys.PublicKeySize]byte
+	copy(id32[:], idBytes)
+
+	hub.ledger.WriteAccountContractCode(id32, []byte("contract code"))
+
+	tests := []struct {
+		name         string
+		url          string
+		wantCode     int
+		wantError    string
+		wantResponse string
+	}{
+		{
+			name:     "missing id",
+			url:      "/contract/",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "id not hex",
+			url:      "/contract/-----",
+			wantCode: http.StatusBadRequest,
+		},
+		{
+			name:     "invalid id length",
+			url:      "/contract/1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d1c331c1d",
+			wantCode: http.StatusBadRequest,
+		},
+		{
+			name:         "valid id",
+			url:          "/contract/" + idHex,
+			wantCode:     http.StatusOK,
+			wantResponse: "contract code",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			request := httptest.NewRequest("GET", tc.url, nil)
+			request.Header.Add(HeaderSessionToken, sess.id)
+
+			w := httptest.NewRecorder()
+
+			hub.router.ServeHTTP(w, request)
+
+			raw, err := ioutil.ReadAll(w.Body)
+			assert.NoError(t, err)
+
+			assert.Equal(t, tc.wantCode, w.Code, "status code")
+
+			if tc.wantResponse != "" {
+				assert.Equal(t, tc.wantResponse, string(raw))
 			}
 		})
 	}
@@ -274,8 +401,10 @@ func TestAuthenticatedAPI(t *testing.T) {
 			{
 				request := httptest.NewRequest(tc.method, tc.url, nil)
 
-				testAuthenticatedAPI(t, hub, request, "session token not specified via HTTP header \"X-Session-Token\"")
-
+				testAuthenticatedAPI(t, hub, request, ErrResponse{
+					StatusText: "Bad request.",
+					ErrorText:  "session token not specified via HTTP header \"X-Session-Token\"",
+				})
 			}
 
 			// With invalid session
@@ -283,37 +412,26 @@ func TestAuthenticatedAPI(t *testing.T) {
 				request := httptest.NewRequest(tc.method, tc.url, nil)
 				request.Header.Add(HeaderSessionToken, "invalid token")
 
-				testAuthenticatedAPI(t, hub, request, "could not find session invalid token")
+				testAuthenticatedAPI(t, hub, request, ErrResponse{
+					StatusText: "Bad request.",
+					ErrorText:  "could not find session invalid token",
+				})
 			}
-
 		})
 	}
 }
 
-func testAuthenticatedAPI(t *testing.T, hub *Hub, request *http.Request, expectedErrorText string) {
+func testAuthenticatedAPI(t *testing.T, hub *Hub, request *http.Request, res ErrResponse) {
 	w := httptest.NewRecorder()
 
 	hub.router.ServeHTTP(w, request)
 
 	raw, err := ioutil.ReadAll(w.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.Nil(t, err)
 
-	if w.Code != http.StatusBadRequest {
-		t.Fatalf("expected status code %d, found %d: %s", http.StatusBadRequest, w.Code, string(raw))
-	}
+	assert.Equal(t, http.StatusBadRequest, w.Code, "status code")
 
-	var response ErrResponse
-	err = json.Unmarshal(raw, &response)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if response.ErrorText != expectedErrorText {
-		t.Fatalf("expected error text `%s`, found `%s`: %s", expectedErrorText, response.ErrorText, string(raw))
-
-	}
+	assert.NoError(t, compareJson(res, raw))
 }
 
 func getGoodCredentialRequest(t *testing.T, keypair *ed25519.Keypair) SessionInitRequest {
@@ -321,9 +439,7 @@ func getGoodCredentialRequest(t *testing.T, keypair *ed25519.Keypair) SessionIni
 	authStr := fmt.Sprintf("%s%d", SessionInitMessage, millis)
 
 	sig, err := eddsa.Sign(keypair.PrivateKey(), []byte(authStr))
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.Nil(t, err)
 
 	return SessionInitRequest{
 		PublicKey:  hex.EncodeToString(keypair.PublicKey()),
@@ -338,4 +454,29 @@ func getBadCredentialRequest() SessionInitRequest {
 		TimeMillis: uint64(time.Now().Unix() * 1000),
 		Signature:  hex.EncodeToString([]byte("bad sig")),
 	}
+}
+
+func compareJson(expected interface{}, found []byte) error {
+	b, err := json.Marshal(expected)
+	if err != nil {
+		return err
+	}
+
+	if bytes.Equal(bytes.TrimSpace(found), b) {
+		return nil
+	}
+
+	return errors.Errorf("expected response `%s`, found `%s`", string(b), string(found))
+}
+
+func createLedger() *wavelet.Ledger {
+	kv := store.NewInmem()
+
+	ledger := wavelet.NewLedger(kv, genesisPath)
+	ledger.RegisterProcessor(sys.TagNop, new(wavelet.NopProcessor))
+	ledger.RegisterProcessor(sys.TagTransfer, new(wavelet.TransferProcessor))
+	ledger.RegisterProcessor(sys.TagContract, new(wavelet.ContractProcessor))
+	ledger.RegisterProcessor(sys.TagStake, new(wavelet.StakeProcessor))
+
+	return ledger
 }
