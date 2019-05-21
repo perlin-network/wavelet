50449cedece32fbb685793f30253c043288c08e5
Andrii Ursulenko <a.ursulenko@gmail.com>
2019-05-21 17:29:47 +0300

    debouncer: add debouncer to timely call handler on number of transactions

---
+++ debouncer/debouncer.go
@@ -0,0 +1,67 @@
+package debouncer
+
+import (
+	"github.com/perlin-network/wavelet"
+	"sync"
+	"time"
+)
+
+type actionHandler func([]*wavelet.Transaction)
+
+type Debouncer struct {
+	buff []*wavelet.Transaction
+	sync.Mutex
+	threshold int
+	action actionHandler
+	stopped bool
+	period time.Duration
+}
+
+func NewDebouncer(threshold int, action actionHandler, period time.Duration) *Debouncer {
+	return &Debouncer{
+		buff: make([]*wavelet.Transaction, 0, threshold),
+		threshold: threshold,
+		action: action,
+		period: period,
+	}
+}
+
+func (d *Debouncer) Start() {
+	timer := time.NewTicker(d.period)
+	for {
+		select {
+		case <-timer.C:
+			d.Lock()
+			d.action(d.buff)
+			d.buff = d.buff[:0]
+			d.Unlock()
+		default:
+		}
+
+		d.Lock()
+		if len(d.buff) == d.threshold {
+			d.action(d.buff)
+			d.buff = d.buff[:0]
+		}
+		d.Unlock()
+	}
+}
+
+func (d *Debouncer) Stop() {
+	d.Lock()
+	d.stopped = true
+	d.Unlock()
+}
+
+func (d *Debouncer) Put(tx *wavelet.Transaction) bool {
+	d.Lock()
+	defer d.Unlock()
+
+	if len(d.buff) >= d.threshold {
+		return false
+	}
+
+	d.buff = append(d.buff, tx)
+
+	return true
+}
---
+++ debouncer/debouncer_test.go
@@ -0,0 +1,43 @@
+package debouncer
+
+import (
+	"github.com/perlin-network/wavelet"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestDebouncerOverfill(t *testing.T) {
+	a := func(txs []*wavelet.Transaction) {}
+
+	d := NewDebouncer(10, a, 1 * time.Second)
+	go d.Start()
+	defer d.Stop()
+
+	for i := 0; i < 10; i++ {
+		d.Put(&wavelet.Transaction{})
+	}
+	assert.False(t, d.Put(&wavelet.Transaction{}))
+}
+
+func TestDebouncer(t *testing.T) {
+	a := func(txs []*wavelet.Transaction) {}
+
+	d := NewDebouncer(100, a, 1 * time.Millisecond)
+	go d.Start()
+	defer d.Stop()
+
+	for i := 0; i < 1000; i++ {
+		assert.True(t, d.Put(&wavelet.Transaction{}), i)
+	}
+}
+
+func BenchmarkDebouncer(b *testing.B) {
+	d := NewDebouncer(40, func([]*wavelet.Transaction){}, 50 * time.Millisecond)
+	go d.Start()
+	defer d.Stop()
+
+	for i := 0; i < b.N; i++ {
+		d.Put(&wavelet.Transaction{})
+	}
+}
