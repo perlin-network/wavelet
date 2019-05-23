32ab264dbdcb52230bd9a42335ef76f0c1acb5da
Kenta Iwasaki <kenta@perlin.net>
2019-05-23 23:44:57 +0800

    ledger, utils: add util method for exporting a ronuds graph in dot format

--- ledger.go
+++ ledger.go
@@ -440,6 +440,8 @@ FINALIZE_ROUNDS:
 			Hex("old_merkle_root", current.Merkle[:]).
 			Uint64("round_depth", finalized.End.Depth-finalized.Start.Depth).
 			Msg("Finalized consensus round, and initialized a new round.")
+
+		//go ExportGraphDOT(finalized, l.graph)
 	}
 }
 
--- utils.go
+++ utils.go
@@ -1,9 +1,14 @@
 package wavelet
 
 import (
+	"encoding/hex"
+	"fmt"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
+	"io/ioutil"
 	"math/rand"
+	"strconv"
+	"strings"
 )
 
 func SelectPeers(peers []*grpc.ClientConn, amount int) ([]*grpc.ClientConn, error) {
@@ -20,4 +25,54 @@ func SelectPeers(peers []*grpc.ClientConn, amount int) ([]*grpc.ClientConn, erro
 	}
 
 	return peers, nil
+}
+
+func ExportGraphDOT(round *Round, graph *Graph) {
+	visited := map[TransactionID]struct{}{round.Start.ID: {}}
+
+	queue := AcquireQueue()
+	defer ReleaseQueue(queue)
+
+	queue.PushBack(&round.End)
+
+	var dot strings.Builder
+
+	dot.WriteString("digraph G {")
+
+	for queue.Len() > 0 {
+		popped := queue.PopFront().(*Transaction)
+
+		for _, parentID := range popped.ParentIDs {
+			if _, seen := visited[parentID]; seen {
+				continue
+			}
+
+			visited[parentID] = struct{}{}
+
+			parent := graph.FindTransaction(parentID)
+
+			if parent == nil {
+				return
+			}
+
+			if parent.Depth <= round.Start.Depth {
+				continue
+			}
+
+			queue.PushBack(parent)
+
+			dot.WriteByte('\n')
+			dot.WriteByte('\t')
+			dot.WriteString(strconv.Quote(hex.EncodeToString(popped.ID[:])))
+			dot.WriteString(" -> ")
+			dot.WriteString(strconv.Quote(hex.EncodeToString(parentID[:])))
+		}
+	}
+
+	dot.WriteByte('\n')
+	dot.WriteByte('}')
+
+	if err := ioutil.WriteFile(fmt.Sprintf("rounds/%d.dot", round.Index), []byte(dot.String()), 0644); err != nil {
+		fmt.Println("error saving graph:", err)
+	}
 }
