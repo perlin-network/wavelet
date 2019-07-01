d850d74dc42a9b694e99b2e1b99630f47c459946
Dowland Aiello <mitsukomegumii@gmail.com>
2019-07-01 00:01:55 -0400

    tx: Update references to NewTransaction to use new signature.

--- graph.go
+++ graph.go
@@ -21,12 +21,13 @@ package wavelet
 
 import (
 	"bytes"
+	"sort"
+	"sync"
+
 	"github.com/google/btree"
 	"github.com/perlin-network/noise/edwards25519"
 	"github.com/perlin-network/wavelet/sys"
 	"github.com/pkg/errors"
-	"sort"
-	"sync"
 )
 
 type GraphOption func(*Graph)
@@ -648,7 +649,7 @@ func (g *Graph) validateTransaction(tx Transaction) error {
 		var nonce [8]byte // TODO(kenta): nonce
 
 		if tx.Sender != tx.Creator {
-			if !edwards25519.Verify(tx.Creator, append(nonce[:], append([]byte{tx.Tag}, tx.Payload...)...), tx.CreatorSignature) {
+			if !edwards25519.Verify(tx.Creator, append(nonce[:], append([]byte{byte(tx.Tag)}, tx.Payload...)...), tx.CreatorSignature) {
 				return errors.New("tx has invalid creator signature")
 			}
 		}
