d67bcdb75f12b42c22b94e89b4c03c1606bf77df
Hasyimi Bahrudin <hasyimibahrudin@gmail.com>
2019-07-23 12:49:02 +0800

    store: improve error handling for NewTestKV

--- store/testutil.go
+++ store/testutil.go
@@ -3,43 +3,34 @@ package store
 import (
 	"os"
 	"testing"
-
-	"github.com/syndtr/goleveldb/leveldb"
 )
 
 // NewTestKV returns a KV store for testing purposes.
 func NewTestKV(t testing.TB, kv string, path string) (KV, func()) {
 	t.Helper()
 
-	if kv == "inmem" {
+	switch kv {
+	case "inmem":
 		inmemdb := NewInmem()
 		return inmemdb, func() {
-			if err := inmemdb.Close(); err != nil {
-				t.Fatal(err)
-			}
-		}
-	}
-	if kv == "level" {
-		// Remove existing db
-		if err := os.RemoveAll(path); err != nil {
-			t.Fatal(err)
+			_ = inmemdb.Close()
 		}
 
-		db, err := NewLevelDB(path)
+	case "level":
+		// Remove existing db
+		_ = os.RemoveAll(path)
+
+		leveldb, err := NewLevelDB(path)
 		if err != nil {
 			t.Fatalf("failed to create LevelDB: %s", err)
 		}
 
-		return db, func() {
-			if err := db.Close(); err != nil && err != leveldb.ErrClosed {
-				t.Fatal(err)
-			}
-			if err := os.RemoveAll(path); err != nil {
-				t.Fatal(err)
-			}
+		return leveldb, func() {
+			_ = leveldb.Close()
+			_ = os.RemoveAll(path)
 		}
-	}
 
-	t.Fatalf("unknown kv %s", kv)
-	panic("unknown kv " + kv)
+	default:
+		panic("unknown kv " + kv)
+	}
 }
