f92ac54cce84aeb29fe33be3779f5c6df5205c30
Sam Sendelbach <sbsends@gmail.com>
2019-07-25 20:16:32 -0500

    ketystore: fix merge conflicts

--- keystore/main_test.go
+++ keystore/main_test.go
@@ -2,21 +2,12 @@ package keystore_test
 
 import (
 	"fmt"
-<<<<<<< HEAD
 	"os"
 	"reflect"
 	"testing"
 
 	"github.com/perlin-network/noise/skademlia"
 	"github.com/perlin-network/wavelet/keystore"
-=======
-	"reflect"
-	"testing"
-
-	"github.com/perlin-network/wavelet/keystore"
-
-	"github.com/perlin-network/noise/skademlia"
->>>>>>> 8f686372a60311817a9e6d13e138d6f1a8fd63f9
 	"github.com/perlin-network/wavelet/sys"
 )
 
@@ -29,7 +20,6 @@ func TestEncryptedKey(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-<<<<<<< HEAD
 	err = keystore.Write("enc.json", enc)
 	if err != nil {
 		t.Error(err)
@@ -60,20 +50,10 @@ func TestPlainTextKeys(t *testing.T) {
 	pt := keystore.NewPlainTextKey(keys.PrivateKey())
 
 	err = keystore.Write("pt.json", pt)
-=======
-	enc.WriteToFile()
-
-	ek, err := keystore.ReadFromFile()
-	if err != nil {
-		t.Error(err)
-	}
-	key, err := ek.Decrypt("test")
->>>>>>> 8f686372a60311817a9e6d13e138d6f1a8fd63f9
 	if err != nil {
 		t.Error(err)
 	}
 
-<<<<<<< HEAD
 	fi, err := os.Stat("pt.json")
 	if err != nil {
 		t.Error(err)
@@ -97,12 +77,3 @@ func TestPlainTextKeys(t *testing.T) {
 // 	}
 
 // }
-=======
-	if !reflect.DeepEqual(keys.PrivateKey(), key) {
-		t.Errorf("decrypted key does not match original key.\nGot: %s \n Want: %s", key, keys.PrivateKey())
-
-	}
-
-	fmt.Println(key)
-}
->>>>>>> 8f686372a60311817a9e6d13e138d6f1a8fd63f9
