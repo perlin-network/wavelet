f12178c50678ccff95914eead1fd2fd2de210e18
Roy Keene <rkeene@users.noreply.github.com>
2019-08-27 07:21:45 -0500

    resolver: write heap pprof to disk upon oom (#186)
    * When the stall detector detects excessive memory usage, also write a binary heap summary
    
    * Close heap file manually
    
    * Wrapped heap writing setup in a lambda
    
    * Move logging of goroutine stack dumps from console to disk along with heap dumps
    
    * Removed erroneous commit
    
    * Remove extraneous whitespace

--- recovery.go
+++ recovery.go
@@ -8,6 +8,8 @@ import (
 	"runtime/pprof"
 	"sync"
 	"time"
+	"fmt"
+	"os"
 )
 
 type StallDetector struct {
@@ -110,9 +112,53 @@ LOOP:
 					if memStats.Alloc > 1048576*d.config.MaxMemoryMB {
 						d.delegate.prepareShutdown(d.mu, errors.New("Memory usage exceeded maximum. Node is scheduled to shutdown now."))
 
-						logger := log.Node()
-						_ = pprof.Lookup("heap").WriteTo(logger, 2)
-						_ = pprof.Lookup("goroutine").WriteTo(logger, 2)
+						func() {
+							// Create directory where we will store the dump
+							// XXX:TODO: Make this user servicable
+							crashDir := "./crashes"
+							if err := os.MkdirAll(crashDir, 0700); err != nil {
+								logger.Error().Err(err).Msgf("Failed to create a directory to record crash logs in: %v", crashDir)
+
+								return
+							}
+
+							// Write out two logs: 1. heap; 2. goroutine, with the
+							// timestamp embedded
+							crashTimestamp := time.Now().Format("2006-01-02-15-04")
+							heapFileName := fmt.Sprintf("%s/heap-%s.pprof", crashDir, crashTimestamp)
+							stackFileName := fmt.Sprintf("%s/goroutine-%s.pprof", crashDir, crashTimestamp)
+
+							// Write out the heap dump
+							heapFile, err := os.Create(heapFileName)
+							if err != nil {
+								logger.Error().Err(err).Msgf("Failed to create pprof file: %v", heapFileName)
+								return
+							}
+
+							defer heapFile.Close()
+
+							err = pprof.Lookup("heap").WriteTo(heapFile, 0)
+							if err != nil {
+								logger.Error().Err(err).Msgf("Failed to write pprof file: %v", heapFileName)
+								return
+							}
+
+							// Write out the goroutine stack dump
+							stackFile, err := os.Create(stackFileName)
+							if err != nil {
+								logger.Error().Err(err).Msgf("Failed to create goroutines stack dump file: %v", stackFileName)
+								return
+							}
+
+							defer stackFile.Close()
+
+							err = pprof.Lookup("goroutine").WriteTo(stackFile, 0)
+							if err != nil {
+								logger.Error().Err(err).Msgf("Failed to write goroutines stack dump file: %v", stackFileName)
+
+								return
+							}
+						}()
 
 						if err := d.tryRestart(); err != nil {
 							logger.Error().Err(err).Msg("Failed to restart process")
