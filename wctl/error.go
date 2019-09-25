17c2a5bf3c9f84663f1f375d22c650838f727441
diamondburned <datutbrus@gmail.com>
2019-09-25 08:36:09 -0700

    Removed commented (*wctl.RequestError).UnmarshalJSON

--- wctl/error.go
+++ wctl/error.go
@@ -44,19 +44,3 @@ func (e *RequestError) Error() string {
 	Request body: %s
 	Response body: %s`, e.StatusCode, e.RequestBody, e.ResponseBody)
 }
-
-/*
-func (e *RequestError) UnmarshalJSON(b []byte) error {
-	var parser fastjson.Parser
-
-	v, err := parser.ParseBytes(b)
-	if err != nil {
-		return err
-	}
-
-	e.Status = string(v.GetStringBytes("status"))
-	e.Error = string(v.GetStringBytes("error"))
-
-	return nil
-}
-*/
