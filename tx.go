1d1e27a218b4173ef0c4847cd94d6b9fa59698e5
Andrii Ursulenko <a.ursulenko@gmail.com>
2019-10-01 20:46:59 +0300

    Change transaction fee and reward calculations. (#239)

--- tx.go
+++ tx.go
@@ -135,6 +135,11 @@ func (tx *Transaction) rehash() {
 	tx.SeedLen = byte(prefixLen(seed))
 }
 
+func (tx Transaction) ComputeSize() int {
+	// TODO: optimize this
+	return len(tx.Marshal())
+}
+
 func (tx Transaction) Marshal() []byte {
 	w := bytes.NewBuffer(make([]byte, 0, 222+(SizeTransactionID*len(tx.ParentIDs))+SizeTransactionSeed*len(tx.ParentSeeds)+len(tx.Payload)))
 
@@ -309,6 +314,15 @@ func (tx Transaction) LogicalUnits() int {
 
 func (tx Transaction) String() string {
 	return fmt.Sprintf("Transaction{ID: %x}", tx.ID)
+}
+
+func (tx Transaction) Fee() uint64 {
+	fee := uint64(sys.TransactionFeeMultiplier * float64(tx.ComputeSize()))
+	if fee < sys.DefaultTransactionFee {
+		return sys.DefaultTransactionFee
+	}
+
+	return fee
 }
 
 func prefixLen(buf []byte) int {
