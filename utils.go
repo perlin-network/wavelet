75093548b3cfe9958240938c434cacf0e6d82dd7
Hasyimi Bahrudin <hasyimi@perlin.net>
2019-10-03 14:31:56 +0800

    utils: readd ExportGraphDOT function

--- utils.go
+++ utils.go
@@ -20,7 +20,12 @@
 package wavelet
 
 import (
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
 	"math/rand"
+	"strconv"
+	"strings"
 
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
@@ -48,4 +53,60 @@ func SelectPeers(peers []*grpc.ClientConn, amount int) ([]*grpc.ClientConn, erro
 	}
 
 	return activePeers, nil
+}
+
+func ExportGraphDOT(round *Round, graph *Graph) {
+	visited := map[TransactionID]struct{}{round.Start.ID: {}}
+
+	queue := AcquireQueue()
+	defer ReleaseQueue(queue)
+
+	queue.PushBack(&round.End)
+
+	var dot strings.Builder
+
+	dot.WriteString("digraph G {")
+
+	for queue.Len() > 0 {
+		popped := queue.PopFront().(*Transaction)
+
+		dot.WriteByte('\n')
+		dot.WriteByte('\t')
+		dot.WriteString(strconv.Quote(hex.EncodeToString(popped.ID[:])))
+		dot.WriteString(" -> ")
+		dot.WriteByte('{')
+		dot.WriteByte(' ')
+
+		for _, parentID := range popped.ParentIDs {
+			if _, seen := visited[parentID]; seen {
+				continue
+			}
+
+			visited[parentID] = struct{}{}
+
+			parent := graph.FindTransaction(parentID)
+
+			if parent == nil {
+				return
+			}
+
+			if parent.Depth <= round.Start.Depth {
+				continue
+			}
+
+			queue.PushBack(parent)
+
+			dot.WriteString(strconv.Quote(hex.EncodeToString(parentID[:])))
+			dot.WriteByte(' ')
+		}
+
+		dot.WriteByte('}')
+	}
+
+	dot.WriteByte('\n')
+	dot.WriteByte('}')
+
+	if err := ioutil.WriteFile(fmt.Sprintf("rounds/%d.dot", round.Index), []byte(dot.String()), 0644); err != nil {
+		fmt.Println("error saving graph:", err)
+	}
 }
