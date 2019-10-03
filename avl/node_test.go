6fbcab60b6bbe396d77faabb8c6900c5a7722bdc
Ahmad Muzakkir <ahmad@perlin.net>
2019-10-03 14:18:02 +0800

    Some micro optimizations in db.go and node.go (#224)
    * db: reduce slice allocations
    
    * node: remove defer since it has overhead.

---
+++ avl/node_test.go
@@ -0,0 +1,32 @@
+package avl
+
+import (
+	"crypto/rand"
+	"github.com/perlin-network/wavelet/store"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func BenchmarkRehashNoWrite(b *testing.B) {
+	kv, cleanup := store.NewTestKV(b, "inmem", "")
+	defer cleanup()
+
+	tree := New(kv)
+
+	var key [32] byte
+	_, err := rand.Read(key[:])
+	assert.NoError(b, err)
+
+	var val [64] byte
+	_, err = rand.Read(val[:])
+	assert.NoError(b, err)
+
+	node := newLeafNode(tree, key[:], val[:])
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for n := 0; n < b.N; n++ {
+		node.rehashNoWrite()
+	}
+}
