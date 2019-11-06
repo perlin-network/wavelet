47501eeaf395482f3bd2aaccd8fe63444ae75e14
Hasyimi Bahrudin <hasyimi@perlin.net>
2019-11-06 16:32:50 +0800

    wctl: remove unused code

--- wctl/counter.go
+++ wctl/counter.go
@@ -1,15 +1,12 @@
 package wctl
 
 import (
-	"sync"
 	"sync/atomic"
 	"unsafe"
 )
 
 type Counter struct {
-	n    uint64
-	lock sync.RWMutex
-	x    [15]byte // instead of "x uint64"
+	x [15]byte // instead of "x uint64"
 }
 
 func (c *Counter) xAddr() *uint64 {
@@ -20,31 +17,15 @@ func (c *Counter) xAddr() *uint64 {
 }
 
 func (c *Counter) Store(val uint64) {
-	// c.lock.Lock()
-	// defer c.lock.Unlock()
-
-	// c.n = val
-
 	p := c.xAddr()
 	atomic.StoreUint64(p, val)
 }
 
 func (c *Counter) Add(delta uint64) uint64 {
-	// c.lock.Lock()
-	// defer c.lock.Unlock()
-
-	// c.n += delta
-	// return c.n
-
 	p := c.xAddr()
 	return atomic.AddUint64(p, delta)
 }
 
 func (c *Counter) Value() uint64 {
-	// c.lock.RLock()
-	// defer c.lock.RUnlock()
-
-	// return c.n
-
 	return atomic.LoadUint64(c.xAddr())
 }
