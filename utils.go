a370dc40b269d36861ecbdd35887f1ef4ac7e06e
Ahmad Muzakkir <ahmad@perlin.net>
2019-11-07 23:56:57 +0800

    utils: add a function to get the closest peer connections with its ID

--- utils.go
+++ utils.go
@@ -20,6 +20,7 @@
 package wavelet
 
 import (
+	"github.com/perlin-network/noise/skademlia"
 	"math/rand"
 
 	"github.com/pkg/errors"
@@ -45,6 +46,51 @@ func SelectPeers(peers []*grpc.ClientConn, amount int) ([]*grpc.ClientConn, erro
 		})
 
 		activePeers = activePeers[:amount]
+	}
+
+	return activePeers, nil
+}
+
+type ClosestPeer struct {
+	conn *grpc.ClientConn
+	id   *skademlia.ID
+}
+
+// Return the list of closest peer's connection and it's ID.
+func ClosestPeers(c *skademlia.Client, amount int, opts ...skademlia.DialOption) ([]ClosestPeer, error) {
+	ids := c.ClosestPeerIDs()
+	if len(ids) < amount {
+		return nil, errors.Errorf("only connected to %d peer(s), but require a minimum of %d peer(s)", len(ids), amount)
+	}
+
+	var peers []ClosestPeer
+
+	for i := range ids {
+		if conn, err := c.Dial(ids[i].Address(), opts...); err == nil {
+			peers = append(peers, ClosestPeer{
+				conn: conn,
+				id:   ids[i],
+			})
+		}
+	}
+
+	activePeers := make([]ClosestPeer, 0, len(peers))
+	for _, p := range peers {
+		if p.conn.GetState() == connectivity.Ready {
+			activePeers = append(activePeers, p)
+		}
+	}
+
+	if len(activePeers) > amount {
+		rand.Shuffle(len(activePeers), func(i, j int) {
+			activePeers[i], activePeers[j] = activePeers[j], activePeers[i]
+		})
+
+		activePeers = activePeers[:amount]
+	}
+
+	if len(activePeers) < amount {
+		return nil, errors.Errorf("only connected to %d peer(s), but require a minimum of %d peer(s)", len(ids), amount)
 	}
 
 	return activePeers, nil
