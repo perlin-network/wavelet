abcde75b67a6e9aba95e1c75aa3b45cff330e5e4
Ahmad Muzakkir <ahmad@perlin.net>
2019-11-11 15:22:05 +0800

    wctl: fix find returned a Transaction when the ID is actually a Contract Account.
    The bug is because, for Contract Account, we use the Transaction ID as its ID.
    So, we need to check for account first, before transaction.

--- wctl/tx_find.go
+++ wctl/tx_find.go
@@ -5,18 +5,16 @@ import "errors"
 // Find queries for either Account or Transaction. Either would be nil. If an
 // error occurs, both will be nil.
 func (c *Client) Find(address [32]byte) (*Account, *Transaction, error) {
+	a, err := c.GetAccount(address)
+	if err == nil {
+		if a.Balance > 0 || a.Stake > 0 || a.Nonce > 0 || a.IsContract || a.NumPages > 0 {
+			return a, nil, nil
+		}
+	}
+
 	t, err := c.GetTransaction(address)
 	if err == nil {
 		return nil, t, nil
-	}
-
-	a, err := c.GetAccount(address)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	if a.Balance > 0 || a.Stake > 0 || a.Nonce > 0 || a.IsContract || a.NumPages > 0 {
-		return a, nil, nil
 	}
 
 	// return nil, nil, err
