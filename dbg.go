833aa6479eaab1c3aedba92b486ac80d5c4cc32d
Kenta Iwasaki <kenta@perlin.net>
2019-12-14 10:37:44 +0800

    dbg: collate logs and record time and order

--- dbg.go
+++ dbg.go
@@ -11,12 +11,34 @@ import (
 var debugMutex sync.Mutex
 var lastDebugLog time.Time
 
+var debugLogsCount map[string]int = make(map[string]int)
+var debugLogsTime map[string]time.Time = make(map[string]time.Time)
+var debugLogsOrder []string
+
 func dbg(args ...interface{}) {
 	debugMutex.Lock()
 	defer debugMutex.Unlock()
 
+	msg := fmt.Sprintln(args...)
+
+	if _, exists := debugLogsCount[msg]; !exists {
+		debugLogsTime[msg] = time.Now()
+		debugLogsOrder = append(debugLogsOrder, msg)
+	}
+
+	debugLogsCount[msg]++
+
 	if time.Since(lastDebugLog).Milliseconds() > 100 {
-		fmt.Println(args...)
+		for _, msg := range debugLogsOrder {
+			fmt.Printf("[x%d] %s", debugLogsCount[msg], msg)
+		}
+
+		for msg := range debugLogsCount {
+			delete(debugLogsCount, msg)
+			delete(debugLogsTime, msg)
+		}
+
+		debugLogsOrder = debugLogsOrder[:0]
 
 		lastDebugLog = time.Now()
 	}
